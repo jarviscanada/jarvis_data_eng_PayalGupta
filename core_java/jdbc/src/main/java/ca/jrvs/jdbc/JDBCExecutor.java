78e1f0c83e7a6218287596838c99a14ef8c5a7e8
Payal Gupta <guptapayal98558@gmail.com>
2021-04-05 14:08:25 -0400

    Data Access files

--- core_java/jdbc/src/main/java/ca/jrvs/jdbc/JDBCExecutor.java
+++ core_java/jdbc/src/main/java/ca/jrvs/jdbc/JDBCExecutor.java
@@ -1,7 +1,24 @@
 package ca.jrvs.jdbc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class JDBCExecutor {
     public static void main(String... args) {
-        System.out.println("Hello learning JDBC");
+        final Logger logger = LoggerFactory.getLogger(JDBCExecutor.class);
+        DatabaseConnectionManager dcm = new DatabaseConnectionManager("172.28.27.43", "hplussport","postgres", "password");
+        try{
+            Connection connection = dcm.getConnection();
+            OrderDAO orderDAO = new OrderDAO(connection);
+            Order order = orderDAO.findById(1000);
+            System.out.println(order);
+        }catch(SQLException e){
+            logger.error("Connection error");
+            }
     }
 }
